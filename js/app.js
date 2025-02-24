500f942f2d297ace3c92271486e2c7ddf6342e57
clairecleverlamb <clairechenszeying@gmail.com>
2025-02-24 13:13:07 -0800

    Improving sound/images features

--- js/app.js
+++ js/app.js
@@ -4,9 +4,27 @@ const notCatDiv = document.querySelector("#not-cat")
 
 let currentAudio = null;
 
-catImg.addEventListener("click", (evt) => {
-    console.log(evt.target)
-})
+//----------------------function to change centerImage--------------
+
+function swapImages(clickedImage) {
+    // Only swap if the clicked image is not already the cat image
+    if (clickedImage !== catImg) {
+        // Store the clicked image's src and alt text
+        const clickedSrc = clickedImage.src;
+        const clickedAlt = clickedImage.alt;
+
+        // Swap the clicked image with the cat image
+        clickedImage.src = catImg.src;
+        clickedImage.alt = catImg.alt;
+
+        // Update the cat image with the previously clicked image's info
+        catImg.src = clickedSrc;
+        catImg.alt = clickedAlt;
+    }
+}
+// catImg.addEventListener("click", (evt) => {
+//     console.log(evt.target)
+// })
 
 catImg.addEventListener("click", (evt) => {
     if(currentAudio){
@@ -27,10 +45,18 @@ notCatDiv.addEventListener("click", (evt) => {
         const audioElement = new Audio(`../assets/audio/${evt.target.id}.mp3`)
         audioElement.volume = .05
         audioElement.play()
-
         currentAudio = audioElement;
+
+        swapImages(evt.target);
     }
 })
+
+document.querySelectorAll("#not-cat img").forEach(img => {
+    img.addEventListener("click", (evt) => {
+        swapImages(evt.target);
+    });
+});
+
 
 
 // Add event listener to <element>
@@ -39,16 +65,3 @@ notCatDiv.addEventListener("click", (evt) => {
 // Get the element id and use it to create an Audio instance/element!
 // Fix its volume
 // Play the audio element!
-
-// const sealionImg = document.querySelector('#sealion')
-// const sealionSays = new Audio('../assets/audio/sealion.mp3')
-
-
-// sealionImg.addEventListener("click", (evt) => {
-//     console.log(evt.target)
-// })
-
-// sealionImg.addEventListener("click", (evt) => {
-//     sealionSays.volume = .05
-//     sealionSays.play()
-// })
